529ce342561e22cce1793b9c253a46dce5090083
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-11-23 03:30:11 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,16 +10,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-// /**
-//    * @type {import('next').NextConfig}
-//    */
-//  const nextConfig   = {
-//   images: {
-//     loader: 'akamai',
-//     path: '',
-//   },
-//   assetPrefix: './',
-// };
-
-// export default   nextConfig;
