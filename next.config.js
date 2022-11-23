af1f03ff73aec8b73a9c154d31d1bcf05f3eab29
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-11-23 03:18:55 +0000

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/**
+   * @type {import('next').NextConfig}
+   */
+ const nextConfig   = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
+
+export default   nextConfig;
